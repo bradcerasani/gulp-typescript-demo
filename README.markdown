04d763f32dcb7802dd08739b816671a5b54c3fbd
Brad Cerasani <bradcerasani@gmail.com>
2014-06-18 19:01:32 -0500

    added browser-sync, updated readme

--- README.markdown
+++ README.markdown
@@ -1,3 +1,10 @@
 # Typescript + Gulp
 
-Basic gulp watch task that compiles TypeScript files.
+Basic gulp watch task that compiles TypeScript files and reloads the browser on change.
+
+### Setup
+
+`git clone https://github.com/bradcerasani/gulp-typescript-demo.git`
+`cd gulp-typescript-demo`
+`npm install`
+`gulp`
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
 var tsc = require('gulp-typescript-compiler');
+var browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function() {
+  browserSync.init(null, {
+    server: {
+      baseDir: "./"
+    }
+  });
+});
 
 gulp.task('typescript', function() {
   return gulp
@@ -7,9 +16,10 @@ gulp.task('typescript', function() {
     .pipe(tsc({
       resolve: true
     }))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.reload({stream:true}));
 });
 
-gulp.task('default', function() {
+gulp.task('default', ['typescript', 'browser-sync'], function() {
   gulp.watch('app/**/*.ts', ['typescript']);
 });
